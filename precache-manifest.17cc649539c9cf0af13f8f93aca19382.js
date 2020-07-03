0d149abe99ca8b32e7c34dcd77650836f5fa17e8
Ravi Kumar G <ravikumarg@maveric-systems.com>
2020-07-03 11:43:40 +0530

    Updates

---
+++ precache-manifest.17cc649539c9cf0af13f8f93aca19382.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a030d8e72e5c828d81d2b353398f9f",
+    "url": "/React-deploy/index.html"
+  },
+  {
+    "revision": "b1d31b19c8f5a38dedac",
+    "url": "/React-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/React-deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1d31b19c8f5a38dedac",
+    "url": "/React-deploy/static/js/main.272c1070.chunk.js"
+  },
+  {
+    "revision": "08a06da5808e43353941",
+    "url": "/React-deploy/static/js/runtime-main.4753e117.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
